0b878ff8c42833a598b2b16f54d59f58725f21ce
Mateo698 <mateora73@gmail.com>
2023-11-14 15:04:43 -0500

    A

---
+++ src/test/java/com/example/ChangedVersion/ChangedVersionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ChangedVersion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChangedVersionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
